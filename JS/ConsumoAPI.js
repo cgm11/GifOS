ffeb1bb0dc1b540c37c78764483ad249b48748b2
Carolina Garcia Montoya <caro.garcia0506@gmail.com>
2020-05-02 20:40:30 -0500

    cambio https por http

--- JS/ConsumoAPI.js
+++ JS/ConsumoAPI.js
@@ -4,7 +4,7 @@ class ConsumoAPI {
     }
 
     subirGif = (file, contenedores, contenedorListo, contenedorSubir, tituloContenedor, consumoApi) => {
-        fetch(`http://upload.giphy.com/v1/gifs`, {
+        fetch(`https://upload.giphy.com/v1/gifs`, {
             method: 'POST',
             mode: 'cors',
             cache: 'no-cache',
@@ -36,7 +36,7 @@ class ConsumoAPI {
 
     urlGifById = () => {
         let id = localStorage.getItem("id's").split(",")[0];
-        let gifById = `http://api.giphy.com/v1/gifs?api_key=${apiKey}&ids=${id}`
+        let gifById = `https://api.giphy.com/v1/gifs?api_key=${apiKey}&ids=${id}`
         const el = document.createElement('textarea');
         el.value = gifById;
         document.body.appendChild(el);
@@ -48,7 +48,7 @@ class ConsumoAPI {
     misGifos = () => {
         let ids = localStorage.getItem("id's");
         console.log(`api.giphy.com/v1/gifs?api_key=${apiKey}&ids=${ids}`);
-        fetch(`http://api.giphy.com/v1/gifs?api_key=${apiKey}&ids=${ids}`)
+        fetch(`https://api.giphy.com/v1/gifs?api_key=${apiKey}&ids=${ids}`)
             .then(response => {
                 return response.json();
             })
